37445d85ef4bf7162c6e058e67101a57600b2241
403-html <tymoteusz.stepien@wp.pl>
2026-05-13 15:57:37 +0200

    feat(matchers): add toHaveQueueSize
    Asserts the number of unconsumed messages currently in the queue.

---
+++ src/matchers/toHaveQueueSize.ts
@@ -0,0 +1,30 @@
+import { MessageQueue } from "../core/queue";
+import { matcherHint, printReceived, printExpected } from "jest-matcher-utils";
+
+export const toHaveQueueSize = function (
+  this: jest.MatcherContext,
+  received: MessageQueue<any>,
+  expectedCount: number,
+) {
+  const queue = received.getQueue();
+  const actualCount = queue.sentMessages.length;
+  const pass = actualCount === expectedCount;
+
+  return {
+    pass,
+    message: () => {
+      const hint = matcherHint(".toHaveQueueSize", "received", "expectedCount");
+      const expectedStr = printExpected(expectedCount);
+      const receivedStr = printReceived(actualCount);
+      const messagesStr = printReceived(queue.sentMessages);
+      return `${hint}
+      
+      Expected queue size:
+        ${expectedStr}
+      Received:
+        ${receivedStr}
+      Queue contents:
+        ${messagesStr}`;
+    },
+  };
+};
