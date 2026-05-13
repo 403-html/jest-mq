032e321820b95384c8a273a438c836745e1b2aac
403-html <tymoteusz.stepien@wp.pl>
2026-05-13 15:57:48 +0200

    feat(matchers): add toHavePublishedTimes
    Asserts a specific message was published exactly n times, counting
    across both sent and received (consumed) messages.

---
+++ src/matchers/toHavePublishedTimes.ts
@@ -0,0 +1,50 @@
+import { MessageQueue } from "../core/queue";
+import { matcherHint, printReceived, printExpected } from "jest-matcher-utils";
+
+export const toHavePublishedTimes = function (
+  this: jest.MatcherContext,
+  received: MessageQueue<any>,
+  expectedMessage: any,
+  expectedCount: number,
+) {
+  const queue = received.getQueue();
+  const allMessages = [
+    ...queue.sentMessages,
+    ...queue.receivedMessages,
+  ];
+  const matchingCount = allMessages.filter((message) =>
+    this.equals(
+      { ...message, id: undefined },
+      expectedMessage,
+    ),
+  ).length;
+  const pass = matchingCount === expectedCount;
+
+  return {
+    pass,
+    message: () => {
+      const hint = matcherHint(
+        ".toHavePublishedTimes",
+        "received",
+        "expectedMessage, expectedCount",
+      );
+      const expectedStr = printExpected({
+        message: expectedMessage,
+        times: expectedCount,
+      });
+      const receivedStr = printReceived({
+        message: expectedMessage,
+        times: matchingCount,
+      });
+      const allMessagesStr = printReceived(allMessages);
+      return `${hint}
+      
+      Expected message published times:
+        ${expectedStr}
+      Received:
+        ${receivedStr}
+      All messages:
+        ${allMessagesStr}`;
+    },
+  };
+};
