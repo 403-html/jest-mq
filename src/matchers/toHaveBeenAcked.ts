4f7f5fd5b9f31e84ae46d25fc9acaacb0dbb3107
403-html <tymoteusz.stepien@wp.pl>
2026-05-13 15:57:25 +0200

    feat(matchers): add toHaveBeenAcked
    Asserts that a message was explicitly acknowledged via queue.ack().
    Does not pass for messages consumed via autoAck.

---
+++ src/matchers/toHaveBeenAcked.ts
@@ -0,0 +1,31 @@
+import { MessageQueue } from "../core/queue";
+import { matcherHint, printReceived, printExpected } from "jest-matcher-utils";
+
+export const toHaveBeenAcked = function (
+  this: jest.MatcherContext,
+  received: MessageQueue<any>,
+  expectedMessage: any,
+) {
+  const queue = received.getQueue();
+  const wasAcked = queue.ackedMessages.some((message) =>
+    this.equals(
+      { ...message, id: undefined },
+      expectedMessage,
+    ),
+  );
+
+  return {
+    pass: wasAcked,
+    message: () => {
+      const hint = matcherHint(".toHaveBeenAcked", "received", "expectedMessage");
+      const expectedStr = printReceived(expectedMessage);
+      const receivedStr = printExpected(queue.ackedMessages);
+      return `${hint}
+
+      Expected message to be acked:
+        ${expectedStr}
+      Acked messages:
+        ${receivedStr}`;
+    },
+  };
+};
