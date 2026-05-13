627a8c70bc9be01317142f0793a193250104a597
403-html <tymoteusz.stepien@wp.pl>
2026-05-13 15:57:51 +0200

    feat(matchers): register toHaveBeenAcked, toHaveReceived, toHaveQueueSize, toHaveOnlyTypes, toHavePublishedTimes

--- src/matchers/index.ts
+++ src/matchers/index.ts
@@ -1,11 +1,21 @@
 import { toBeInQueue } from "./toBeInQueue";
 import { toHaveEmptyQueue } from "./toHaveEmptyQueue";
+import { toHaveBeenAcked } from "./toHaveBeenAcked";
+import { toHaveOnlyTypes } from "./toHaveOnlyTypes";
+import { toHavePublishedTimes } from "./toHavePublishedTimes";
+import { toHaveQueueSize } from "./toHaveQueueSize";
+import { toHaveReceived } from "./toHaveReceived";
 
 declare global {
   namespace jest {
     interface Matchers<R> {
       toBeInQueue(expectedMessage: any): R;
       toHaveEmptyQueue(): R;
+      toHaveBeenAcked(expectedMessage: any): R;
+      toHaveOnlyTypes(expectedTypes: string[]): R;
+      toHavePublishedTimes(expectedMessage: any, expectedCount: number): R;
+      toHaveQueueSize(expectedCount: number): R;
+      toHaveReceived(expectedMessage: any): R;
     }
   }
 }
@@ -13,4 +23,9 @@ declare global {
 expect.extend({
   toBeInQueue,
   toHaveEmptyQueue,
+  toHaveBeenAcked,
+  toHaveOnlyTypes,
+  toHavePublishedTimes,
+  toHaveQueueSize,
+  toHaveReceived,
 });
